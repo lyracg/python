01f5bf789ae7b02d66551a049942d06d28531041
lyracg <pedro.romero@ine.mx>
2026-09-01 09:02:47 -0600

    Aquí escribes tu mensaje sobre el cambio

---
+++ CALCULADORA DE ÍNDICE DE MASA CORPORAL (IMC).py
@@ -0,0 +1,9 @@
+
+
+peso = float(input("Escribe tu peso en kl: "))
+
+altura = float(input("Escribe tu altura en metros: "))
+
+imc = peso / (altura ** 2)
+
+print(f"Tu indice de masa corporal (IMC) es: {imc:.1f}")
